6d3c9a97fc41b498af538b82335312f49ebaf2fe
stas.dovgodko <stas.dovgodko@webmaniacs.net>
2015-01-11 17:40:38 +0200

    Refactor + debug

--- sketch/alpha1/alpha1.ino
+++ sketch/alpha1/alpha1.ino
@@ -1,45 +1,23 @@
-#define headphonesOff 3;
-#define headphonesOn 30;
+#define headphonesControlPin 7 // headphones pin (should be open for connected HPs )
+#define outputRelayPin 2 // output switch relay
+#define headphonesPowerRelayPin 3 // output switch relay
 
-int buttonState = 0;
-int headphonesRelayState = 0;
+
+const int OUTPUT_TO_HP_POWER_DELAY = 1000; // delay to power off or switch output
 
 void setup() {
-  // put your setup code here, to run once:
-// Инициализируем цифровой вход/выход в режиме выхода.
-  // Выход 13 на большинстве плат Arduino подключен к светодиоду на плате.
-  pinMode(13, OUTPUT);
- pinMode(2, OUTPUT);
- pinMode(3, OUTPUT);
-pinMode(7, INPUT); 
+  Serial.begin(9600);
+  
+  String pinPrefix = "#";
+  
+  Serial.println("DEBUG: HP control pin " + pinPrefix + headphonesControlPin);
+  Serial.println("DEBUG: Output relay pin " + pinPrefix + outputRelayPin);
+  Serial.println("DEBUG: HP amp power relay pin " + pinPrefix + headphonesPowerRelayPin);
+  pinMode(headphonesControlPin, INPUT_PULLUP);
+  pinMode(outputRelayPin, OUTPUT);
+  pinMode(headphonesPowerRelayPin, OUTPUT);
 }
 
 void loop() {
-  buttonState = digitalRead(7);
- 
-  // проверяем нажата ли кнопка
-  // если нажата, то buttonState будет HIGH:
-  if (buttonState == HIGH) {   
-    // включаем светодиод   
-    digitalWrite(2, HIGH);
-   digitalWrite(3, HIGH); 
-  }
-  else {
-    // выключаем светодиод
-    digitalWrite(2, LOW);
-    digitalWrite(3, LOW);
-  }
-
-  //digitalWrite(2, HIGH);   // зажигаем светодиод
-  //delay(500);              // ждем секунду
-  //digitalWrite(2, LOW);
-  // put your main code here, to run repeatedly:
-  //digitalWrite(13, HIGH);   // зажигаем светодиод
-  //delay(500);              // ждем секунду
-  //digitalWrite(13, LOW);  // выключаем светодиод
-  //delay(500);              // ждем секунду
-  //digitalWrite(13, HIGH);  // выключаем светодиод
-  //delay(500);              // ждем секунду
-  //digitalWrite(13, LOW);  // выключаем светодиод
-  //delay(2000);              // ждем секунду
+  checkHeaphones(headphonesControlPin, outputRelayPin, headphonesPowerRelayPin);
 }
